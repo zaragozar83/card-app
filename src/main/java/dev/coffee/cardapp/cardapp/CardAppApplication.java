28ad2b947b37e45bb5b4ffcca473e6596692c9b3
rzaragoza <ricardo.zaragoza@riac.dev>
2022-09-21 10:50:31 -0500

    First commit

---
+++ src/main/java/dev/coffee/cardapp/cardapp/CardAppApplication.java
@@ -0,0 +1,13 @@
+package dev.coffee.cardapp.cardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/coffee/cardapp/cardapp/CardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.coffee.cardapp.cardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
